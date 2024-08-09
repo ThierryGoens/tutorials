25ee1b0ed2384a51d9f275f79bb3658ce0049ec0
ThierryGoens <thierry@ignitia.be>
2024-08-09 12:43:05 +0200

    fin chap1 pt4

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,7 +1,16 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
+import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
+
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+
+    static components ={ Counter, Card }
+
+    value1 = "<div>some text 1</div>";
+    value2 = markup("<div>some text 2</div>");
+
 }
